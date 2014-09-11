6ad6c430e82093ce910ea34366f624396de8c202
Sean Lin <seanl@intersect.org.au>
2014-09-12 09:42:51 +1000

    Fix ITEM_QUERY: filter regex only works with raw string. The ?corpus is a rdflib.term.URIRef(u'http://ns.ausnc.org.au/corpora/mbep') which does not work with SPARQL regex filter

--- hcsvlab_robochef/utils/manifester.py
+++ hcsvlab_robochef/utils/manifester.py
@@ -14,7 +14,7 @@ ITEM_QUERY = """SELECT DISTINCT ?item ?identifier
 				   WHERE {
 			   		?item <http://purl.org/dc/terms/identifier> ?identifier .
 			   		?item <http://purl.org/dc/terms/isPartOf> ?corpus .
-			   		FILTER regex(?corpus, "COLL")
+			   		FILTER ( regex(str(?corpus), "COLL") )
 		   		}"""
 
 DOCUMENT_QUERY = """SELECT DISTINCT ?type ?identifier ?source
@@ -26,55 +26,56 @@ DOCUMENT_QUERY = """SELECT DISTINCT ?type ?identifier ?source
 
 
 def create_manifest(srcdir, format):
-	rdf_files = get_files(srcdir)
-	manifest_hash = {"collection_name":extract_manifest_collection(rdf_files[0], format), "files":{}}
-	
-	total = len(rdf_files)
-	sofar = 0
+    rdf_files = get_files(srcdir)
+    manifest_hash = {"collection_name": extract_manifest_collection(rdf_files[0], format), "files": {}}
 
-	for rdf in rdf_files:
-		filename = os.path.basename(rdf)
+    total = len(rdf_files)
+    sofar = 0
 
-		graph = Graph()
-		graph.parse(rdf, format=format)
+    for rdf in rdf_files:
+        filename = os.path.basename(rdf)
 
-		res = graph.query(ITEM_QUERY.replace("COLL", manifest_hash["collection_name"]))
-		for row in res:
-			entry = {"id":str(row[1]), "uri":str(row[0]), "docs":[]}
-			break
+        graph = Graph()
+        graph.parse(rdf, format=format)
+        res = graph.query(ITEM_QUERY.replace("COLL", manifest_hash["collection_name"]))
+        for row in res:
+            entry = {"id": str(row[1]), "uri": str(row[0]), "docs": []}
+            break
 
-		res = graph.query(DOCUMENT_QUERY)
-		for row in res:
-			entry["docs"].append({"identifier":str(row[1]), "source":str(row[2]), "type":str(row[0])})
+        res = graph.query(DOCUMENT_QUERY)
+        for row in res:
+            entry["docs"].append({"identifier": str(row[1]), "source": str(row[2]), "type": str(row[0])})
 
-		manifest_hash["files"][filename] = entry
+        manifest_hash["files"][filename] = entry
 
-		sofar = sofar + 1
-		print "\033[2K   ", sofar, "of", total, os.path.basename(rdf), "\033[A"
+        sofar = sofar + 1
+        print "\033[2K   ", sofar, "of", total, os.path.basename(rdf), "\033[A"
 
-	with open(os.path.join(srcdir, "manifest.json"), 'w') as outfile:
-		json.dump(manifest_hash, outfile)
+    with open(os.path.join(srcdir, "manifest.json"), 'w') as outfile:
+        json.dump(manifest_hash, outfile)
 
-	print "\033[2K   ", total, "files processed"
+    print "\033[2K   ", total, "files processed"
+
 
 def get_files(srcdir):
-	return_files = []
+    return_files = []
 
-	for root, dirnames, filenames in os.walk(srcdir):
-		for filename in fnmatch.filter(filenames, '*-metadata.rdf'):
-			return_files.append(os.path.join(root, filename))
+    for root, dirnames, filenames in os.walk(srcdir):
+        for filename in fnmatch.filter(filenames, '*-metadata.rdf'):
+            return_files.append(os.path.join(root, filename))
 
-	return return_files
+    return return_files
+
 
 def extract_manifest_collection(rdf_file, format):
-	graph = Graph()
-	graph.parse(rdf_file, format=format)
-	
-	res = graph.query(COLLECTION_QUERY)
-	# Small hack for austalk
-	for row in res:
-		if str(row[0]) == "http://ns.austalk.edu.au/corpus":
-			return "austalk"
-	for row in res:
-		return os.path.basename(row[0])
-	return ""
+    graph = Graph()
+    graph.parse(rdf_file, format=format)
+
+    res = graph.query(COLLECTION_QUERY)
+    # Small hack for austalk
+    for row in res:
+        if str(row[0]) == "http://ns.austalk.edu.au/corpus":
+            return "austalk"
+    for row in res:
+        return os.path.basename(row[0])
+    return ""
